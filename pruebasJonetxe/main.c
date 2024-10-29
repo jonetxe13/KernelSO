be07b43667b7c43f4d546bd52feb71b46deac1c8
jonetxe13 <jonetxe.sasia13@gmail.com>
2024-10-29 18:47:10 +0100

    pruebas del scheduler con hilos bastante malamente

--- pruebasJonetxe/main.c
+++ pruebasJonetxe/main.c
@@ -2,16 +2,26 @@
 #include <linux/sched.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
 
-// void scheduler_call();
 // Estructura para el PCB (Process Control Block)
 typedef struct task_struct {
     int pid;                // Identificador del proceso
-    struct PCB *siguiente;  // Puntero al siguiente proceso en la cola
+    struct task_struct *siguiente;  // Puntero al siguiente proceso en la cola
 } PCB;
+
+typedef struct { 
+    pthread_t thread; 
+    int id; 
+} Thread;
+
+typedef struct { 
+    Thread* threads; 
+    int num_threads; 
+} ThreadArgs;
 
 // Variables globales
 PCB *colaProcesos = NULL;
@@ -21,24 +31,53 @@ pthread_cond_t cond2 = PTHREAD_COND_INITIALIZER;
 int num_processes = 0;
 int ticks = 0;
 
-void* scheduler(void* threadid){
-    long tid = (long)threadid;
+void *PrintTask(void *processId) { 
+    int process= *(int *)processId; 
+    printf("Hello World! It's me, thread #%d!\n", process); 
+    sleep(3); // simulación de la duración del proceso
+    free(processId);
+    pthread_exit(NULL); 
+}
+
+void* scheduler(void* args){
+    ThreadArgs *threadArgs = (ThreadArgs *) args; 
+    Thread *threadArray = threadArgs->threads;
+    int num_threads = threadArgs->num_threads;
     while (1) {
         pthread_mutex_lock(&mutex);
         pthread_cond_wait(&cond2, &mutex);
         int index = 0;
-        while( index < num_processes){
-            if (index >= num_processes) {
-                pthread_mutex_unlock(&mutex);
-                break;
+        int threadNum = 0;
+        // while( index < num_processes){
+        //     PCB p = colaProcesos[index];
+        //     int *pid_copy = malloc(sizeof(int));
+        //     *pid_copy = p.pid;
+        //
+            printf("Scheduler: num_processes = %d, num_threads = %d\n", num_processes, threadNum);
+            PCB *temp = colaProcesos; 
+            while (temp != NULL) { 
+                int *pid_copy = malloc(sizeof(int)); 
+                *pid_copy = temp->pid; 
+                if (threadNum < num_threads) { 
+                        pthread_create(&threadArray[threadNum].thread, NULL, PrintTask, (void*) &pid_copy); 
+                        printf("Scheduler: Created thread %d for process %d\n", threadNum, temp->pid); 
+                        threadNum++; 
+                } 
+                temp = temp->siguiente; 
+            } 
+            if (threadNum == num_threads) { 
+                printf("Scheduler: CPU is full with %d threads.\n", num_threads); 
             }
-            PCB p = colaProcesos[index];
-
-            printf("Un thread #%ld está ejecutando el proceso #%d\n", tid, p.pid);
-            index++;
-        }
+            // if(threadNum < num_threads){
+            //     pthread_create(&threadArray[threadNum].thread, NULL, PrintTask,(void*) &pid_copy);
+            //     printf("Scheduler: Created thread %d for process %d\n", threadNum,p.pid);
+            //     threadNum++;
+            // }else{
+            //     printf("el maximo de procesos ya esta ejecutandose");
+            // }
+            // index++;
+        // }
         pthread_mutex_unlock(&mutex);
-        sleep(3); // simulación de la duración del proceso
     }
     pthread_exit(NULL);
     return NULL;
@@ -57,12 +96,12 @@ void* generarProcesos(void* arg) {
         colaProcesos = nuevoProceso;
         printf("Generado proceso con PID %d\n", nuevoProceso->pid);
         
-        PCB *temp = colaProcesos;
-        printf("Lista de procesos:\n");
-        while (temp != NULL) {
-            printf("PID %d\n", temp->pid);
-            temp = temp->siguiente;
-        }
+        // PCB *temp = colaProcesos;
+        // printf("Lista de procesos:\n");
+        // while (temp != NULL) {
+        //     printf("PID %d\n", temp->pid);
+        //     temp = temp->siguiente;
+        // }
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
@@ -101,14 +140,16 @@ void* clockNuestro(){
 }
 
 int main(int argc, char** argv) {
-    srand(time(NULL));
-    pthread_t thread[atoi(argv[2])];
+    // pthread_t hilos[atoi(argv[3])];
+    Thread threads[atoi(argv[2])];
+    ThreadArgs threadArgs = {threads, atoi(argv[2])};
+    
     if (argc == 1){
         printf("tienes que meter un argumento melon");
         exit(1);
     }
     float frequencia = atof(argv[1]);
-    pthread_t relojThread, timerThread, procesoThread;
+    pthread_t relojThread, timerThread, procesoThread, schedulerThread;
 
     // Crear threads
     if (pthread_create(&relojThread, NULL, clockNuestro, NULL)) {
@@ -123,15 +164,11 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Error creando el thread del generador de procesos\n");
         return 1;
     }
-    for (int i =0; i<atoi(argv[2]); i++){
-        if (pthread_create(&thread[i], NULL, scheduler, NULL)) {
-            fprintf(stderr, "Error creando el thread de un proceso");
-            return 1;
-        }
+    if (pthread_create(&schedulerThread, NULL, scheduler, &threadArgs)) {
+        fprintf(stderr, "Error creando el thread de un proceso");
+        return 1;
     }
-    for (int i =0; i<atoi(argv[2]); i++){
-        pthread_join(thread[i], NULL);
-    }
+    pthread_join(schedulerThread, NULL);
     // Unirse a los threads (para que el programa no termine inmediatamente)
     pthread_join(relojThread, NULL);
     pthread_join(timerThread, NULL);
